265d004fd95933682edcdcb5104261e67ee837f1
hugoadriao <hugovictorsilva94@gmail.com>
2018-09-06 11:24:50 -0300

    Itens, mudança no algoritmo, algoritmo de itens aleatórios

--- random_item.py
+++ random_item.py
@@ -1,7 +1,7 @@
 def random_item(n, ini, fin):
     import random
     
-    with open(f'{n}_item.txt','w') as f:
+    with open(f'{n}_atividade.txt','w') as f:
         for i in range(1, n, 1):
             f.write( str((random.randint(ini, fin),random.randint(ini, fin)) ))
             f.write('\n')
